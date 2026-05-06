7729297dcf0a66960d8ecd065036b0efd8845c73
RickDev <henriiisilva2007@gmail.com>
2026-05-05 22:01:00 -0300

    Primeiro commit

---
+++ src/test/java/com/estudos/Vagas/VagasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.Vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
