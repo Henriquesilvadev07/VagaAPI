a80e70377737ac3cf9ea76db34a939a23a5af162
RickDev <henriiisilva2007@gmail.com>
2026-07-01 22:00:11 -0300

    Criaçao da Entity usuarios para validacao com security

---
+++ src/main/java/com/estudos/Vagas/Users/UsuarioModel.java
@@ -0,0 +1,29 @@
+package com.estudos.Vagas.Users;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Usuario")
+@Table(name = "usuarios")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true)
+    @NotBlank(message = "é necessário colocar o login")
+    private String login;
+
+    @NotBlank(message = "é necessário colocar a senha")
+    private String senha;
+
+
+
+}
