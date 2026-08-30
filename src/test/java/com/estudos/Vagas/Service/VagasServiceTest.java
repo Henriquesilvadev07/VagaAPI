1836bf3255e501895d6ea2283e9404936a4e7602
RickDev <henriiisilva2007@gmail.com>
2026-08-29 21:48:42 -0300

    Criacao de cenario 1 de teste

--- src/test/java/com/estudos/Vagas/Service/VagasServiceTest.java
+++ src/test/java/com/estudos/Vagas/Service/VagasServiceTest.java
@@ -1,7 +1,14 @@
 package com.estudos.Vagas.Service;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class VagasServiceTest {
+
+    @Test
+    void cenari01() {
+
+    }
 
 }
